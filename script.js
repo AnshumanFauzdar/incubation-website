ba8ecb160472b224c8cfbd60257aab193b96642d
Anshuman Fauzdar <fauzdaranshuman@gmail.com>
2020-03-22 15:46:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Preloader Animation
+
 $(window).on('load', function() { // makes sure the whole site is loaded
-  $('#status').delay(3500).fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(3500).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').delay(3600).css({'overflow':'visible'});
+  $('#status').delay(2500).fadeOut(); // will first fade out the loading animation
+  $('#preloader').delay(2500).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(2600).css({'overflow':'visible'});
 })
