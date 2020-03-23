8d9a3f3c46551a53d1754753acca0e8ba128da19
sunnyvedwal <sunnyvedwal@gmail.com>
2020-03-23 18:18:34 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-$(window).on('load', function() { // makes sure the whole site is loaded
-  $('#status').delay(3500).fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(3500).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').delay(3600).css({'overflow':'visible'});
-})
